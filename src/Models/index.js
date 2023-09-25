f7faaa51a6759762e965b4c8363e76f05d87144d
shema paulin <shema@post.com>
2023-09-25 15:57:19 +0200

    updated

--- src/Models/index.js
+++ src/Models/index.js
@@ -1,17 +1,12 @@
 import {Sequelize} from "sequelize";
 import config from "../config/config.js";
 
-const env = process.env.NODE_ENV || 'development';
 
-const configer=config[env];
 
 let database;
-if(configer.url){
-database=new Sequelize(configer.url,{
+
+database=new Sequelize(config.development.url,{
     dialect:"mysql"
-});
-}else {
-   
-  }
+})
 
 export default  database;
